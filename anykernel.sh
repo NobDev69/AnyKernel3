fbf7465f6b8a82e894d497dc491dc933e6de8448
Md. Kashem <167101304+NobDev69@users.noreply.github.com>
2025-01-04 11:26:44 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Archipelago Kernel by Gabuters-dev
+kernel.string=Nethunter Kernel by NobDev69
 do.devicecheck=1
 do.modules=0
 do.systemless=1
